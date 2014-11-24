476dba6ab2c8c6aa59116e3f17f719c326b0dfca
Remy DESTREBECQ <rdestreb@e3r8p16.42.fr>
2014-11-24 16:47:28 +0100

    no modifs .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rdestreb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/15 13:48:03 by rdestreb          #+#    #+#             */
-/*   Updated: 2014/11/24 12:08:28 by rdestreb         ###   ########.fr       */
+/*   Updated: 2014/11/24 14:24:16 by rdestreb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define GET_NEXT_LINE_H
 
 # define BUFF_SIZE 42
+# include <sys/types.h>
+# include <sys/uio.h>
 # include <unistd.h>
 # include "libft/libft.h"
 
